10aeac1040ac9ba156ae70b1e174a9c7fa9b4393
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2023-11-14 23:27:50 +0000

    IDK

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
@@ -33,14 +33,41 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>
+          ${
+            variables.name == null || variables.name == ""
+              ? "First"
+              : variables.name
+          }
+          ${
+            variables.lastName == null || variables.lastName == ""
+              ? "Last"
+              : variables.lastName
+          }
+          </h1>
+          <h2>
+          ${variables.role == null ? "Role" : variables.role}
+          </h2>
+          <h3>
+          ${variables.city == null ? "City, Country" : variables.city}
+          ${variables.city == "Miami" ? ", USA" : ""}
+          ${variables.city == "Toronto" ? ", CA" : ""}
+          ${variables.city == "Amsterdam" ? ", NL" : ""}
+          </h3>
+          
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "#" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "#" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/In/${
+              variables.linkedin == null ? "#" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "#" : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
